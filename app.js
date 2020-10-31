53ceb866f7f11ac87d5d93d4b7d6348ef0ded1f9
chromakeeey <chromakeeey@gmail.com>
2020-10-31 18:15:26 +0200

    add start page

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ app.use('/api/', require('./routes/user.route'))
 app.use('/api/', require('./routes/subject.route'))
 app.use('/api/', require('./routes/specialty.route'))
 
+app.get('/', (req, res) => {
+    res.end('<h1>College site API</h1>')
+})
+
 app.listen(PORT, () => {
     console.log(`App has been started on port ${PORT}...`)
 });
