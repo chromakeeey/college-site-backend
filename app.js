5fe45b539c134f8c0bca149909e2d2e85b835e27
Namnetsy <namnetsy@gmail.com>
2020-12-05 11:26:20 -0500

    update .env.example

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(Session({
             const RedisStore = require('./sessions/RedisStore');
             const Redis = require('ioredis');
 
-            return new RedisStore(new Redis({
+            return new RedisStore(new Redis(process.env.REDIS_URL, {
                 showFriendlyErrorStack: true
             }));
         }
